f1acc67141837049c3792adfb9758eb434bc17bb
RaliZen <ralitsa.tsenkova@gmail.com>
2019-01-22 17:21:43 +0100

    Check if Logstash 2.4.0 installed, if not download and install

--- run_LogstashTest.sh
+++ run_LogstashTest.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-cd /tmp
-
+cd ~
+#Check if logstash 2.4.0 is installed on this PC
+if [ -d "./work/logstash-2.4.0/" ]
+then
+	echo "Logstash 2.4.0 already installed"
+else
+	# Create work folder
+	mkdir work
+	cd work
+	#Download and install Logstash 2.4.0
+   	echo "Logstash 2.4.0 is being installed"
+   	wget https://download.elastic.co/logstash/logstash/logstash-2.4.0.tar.gz
+   	tar -xzf logstash-2.4.0.tar.gz
+fi
 # Check if Logstash_Test exists 
-
 LST="/tmp/work/Logstash_Test/"
 if [ -d "$LST" ]
 then
-cd $LST
-# Update project    
-   git pull origin master 
+	cd $LST
+	# Update project    
+   	git pull origin master 
 else
-   
-# Create work folder
- 
-   mkdir work
-   cd work
-
-# Download project
-
-   git clone  https://github.com/RaliZen/Logstash_Test.git
-   
-# Create data storage folder    
-   mkdir lst_reports
+	# Create work folder
+        mkdir work
+        cd work
+	# Download project
+	git clone  https://github.com/RaliZen/Logstash_Test.git
+	# Create data storage folder    
+   	mkdir lst_reports
 fi    
 
 cd /tmp/work/Logstash_Test
@@ -30,21 +36,20 @@ FSZ=$(stat -c %s sample_orig.json)
 echo $FSZ
 
 # Check if sincedb exists
-
 DST="/tmp/work/lst_reports/sincedb_sample_orig"
 if [ -f "$DST" ] 
 then  
-   echo "sincedb exists alreaday"
-# Remove sincedb
-   cd /tmp/work/lst_reports
-   echo "Removing sincedb"
-   rm sincedb_sample_orig
+	echo "sincedb exists alreaday"
+	# Remove sincedb
+   	cd /tmp/work/lst_reports
+   	echo "Removing sincedb"
+   	rm sincedb_sample_orig
 fi
 
 # Switch to home folder
 cd ~
 # Run Logstash with the provided data
-./logstash-2.4.0/bin/logstash -f "/tmp/work/Logstash_Test/test_orig_sj.conf"&
+./work/logstash-2.4.0/bin/logstash -f "/tmp/work/Logstash_Test/test_orig_sj.conf"&
 
 PID=`pgrep logstash`
 echo $PID
@@ -52,13 +57,13 @@ echo $PID
 cd /tmp/work/lst_reports
 until [ -f sincedb_sample_orig ]
 do
- sleep 15
+ 	sleep 15
 done
 
 if grep -q $FSZ sincedb_sample_orig
 then	
-  kill -s SIGTERM $PID&
-echo "Logstash terminated"
+  	kill -s SIGTERM $PID&
+	echo "Logstash terminated"
 fi
 
 
