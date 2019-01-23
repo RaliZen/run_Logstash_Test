adf73734b4da0dc5c39b7018ef962d13fa223923
RaliZen <ralitsa.tsenkova@gmail.com>
2019-01-23 19:01:00 +0100

    Java version test and setup added

--- run_LogstashTest.sh
+++ run_LogstashTest.sh
@@ -1,7 +1,54 @@
 #!/bin/bash
 
+# Switch to home folder
 cd ~
-#Check if logstash 2.4.0 is installed on this PC
+# Get user
+User=$(whoami)
+# Check Java version, then extract only the relevant numbers
+JV=$( java -version 2>&1 | grep -o -E '[0-9,.,_]+'| head -n1 )
+JVN=$( java -version 2>&1 | tr -dc '0-9' |  cut -c -6  )
+
+if [ -z $JVN  ]
+then
+        echo "Installing OpenJDK 1.8.0_191. During the setup you will be prompted to enter your root password."
+        # Install Java 8
+        read -s -p "Enter your password for sudo: " sudoPW
+        echo $sudoPW | sudo -u $User
+        sudo apt-get install openjdk-8-jre-headless
+elif [ $JVN -lt 180191 ]
+then
+        if [ -d "/usr/lib/jvm/java-8-openjdk-amd64/" ]
+        then
+                echo "Switching OpenJDK Version 1.8.0_191"
+                read -s -p "Enter your password for sudo: " sudoPW
+                echo $sudoPW | sudo -u $User
+                echo 2 | sudo update-alternatives --config java
+                #sudo echo 2 | update-alternatives --config java
+                #sudo update-java-alternatives --set /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+        else
+                echo "You are running OpenJDK Version $JV. To run Logstash you need Version 1.8.0_191 or lower"
+                read -p "Do you want me to install and setup OpenJDK Version 1.8.0_191[y/n]?" answer
+                if [ $answer == "y" ]
+                then
+                        echo "During the setup you will be prompted to enter your root passwort"
+                        #Install Java 8
+                        echo "OpenJDK 1.8.0_191 is being installed"
+                        read -s -p "Enter your password for sudo: " sudoPW
+                        echo $sudoPW | sudo -u $User
+                        sudo apt-get install openjdk-8-jre-headless
+                        #sudo update-java-alternatives --set /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+                        #sudo echo 2 | update-alternatives --config java
+                        echo 2 | sudo update-alternatives --config java
+                 else
+                        echo "Can not run Logstash Test \ Exiting"
+                fi
+        fi
+fi
+
+# Revoking sudo rights 
+
+sudo -k
+# Check if logstash 2.4.0 is installed on this PC
 if [ -d "./work/logstash-2.4.0/" ]
 then
 	echo "Logstash 2.4.0 already installed"
@@ -14,6 +61,9 @@ else
    	wget https://download.elastic.co/logstash/logstash/logstash-2.4.0.tar.gz
    	tar -xzf logstash-2.4.0.tar.gz
 fi
+
+# Switch to root folder 
+cd /tmp/
 # Check if Logstash_Test exists 
 LST="/tmp/work/Logstash_Test/"
 if [ -d "$LST" ]
@@ -30,11 +80,10 @@ else
 	# Create data storage folder    
    	mkdir lst_reports
 fi    
-
 cd /tmp/work/Logstash_Test
+# Check the size of the input file
 FSZ=$(stat -c %s sample_orig.json)
 echo $FSZ
-
 # Check if sincedb exists
 DST="/tmp/work/lst_reports/sincedb_sample_orig"
 if [ -f "$DST" ] 
